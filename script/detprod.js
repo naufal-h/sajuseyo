0eef4a30987bc696d4dd871851e7bb07ea53b98b
naufal-h <naufal.humam-2021@fst.unair.ac.id>
2023-03-25 15:48:31 +0700

    added js for detprod

--- script/detprod.js
+++ script/detprod.js
@@ -14,3 +14,39 @@ function hoverOut() {
     document.querySelector(".star" + i).src = "/assets/svg/star.svg";
   }
 }
+
+function categoryImg(pic, btnId) {
+  document.querySelector(".big-img img").src = pic;
+  document.querySelectorAll(".category-buttons").forEach((button) => {
+    if (button.id === btnId) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+}
+
+let qty = 1;
+const unitPrice = 350000;
+
+function increaseQty() {
+  qty++;
+  document.querySelector(".qty").innerText = qty;
+  document.querySelector(".total-qty").innerText = qty;
+  document.getElementById("totalPrice").innerHTML =
+    "Rp. " + formatPrice(qty * unitPrice);
+}
+
+function decreaseQty() {
+  if (qty > 1) {
+    qty--;
+    document.querySelector(".qty").innerText = qty;
+    document.querySelector(".total-qty").innerText = qty;
+    document.getElementById("totalPrice").innerHTML =
+      "Rp. " + formatPrice(qty * unitPrice);
+  }
+}
+
+function formatPrice(price) {
+  return price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+}
