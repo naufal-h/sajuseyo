f2f3055be8deb0e3db7d7b718e6388a6587e0dbd
naufal-h <naufal.humam-2021@fst.unair.ac.id>
2023-03-25 23:11:22 +0700

    added navigator

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ class Footer extends HTMLElement {
       <div class="footer-container">
         <div class="footer-grid">
           <div class="footer-logo-block">
-            <a href="/home.html" class="footer-logo">
+            <a href="home.html" class="footer-logo">
               <img src="/assets/logo/logo-black.png" width="304" />
             </a>
             <p class="tag-line">
@@ -19,23 +19,23 @@ class Footer extends HTMLElement {
           </div>
           <div class="footer-links-container">
             <h5 class="footer-header">Product</h5>
-            <a href="/music" class="footer-link">Music</a>
-            <a href="/fanlight" class="footer-link">Fanlight</a>
-            <a href="/photobook" class="footer-link">Photo Book</a>
-            <a href="/printedphoto" class="footer-link">Printed Photo</a>
+            <a href="product.html" class="footer-link">Music</a>
+            <a href="product.html" class="footer-link">Fanlight</a>
+            <a href="product.html" class="footer-link">Photo Book</a>
+            <a href="product.html" class="footer-link">Printed Photo</a>
           </div>
           <div class="footer-links-container">
             <h5 class="footer-header">Celeb</h5>
-            <a href="/" class="footer-link">SM Ent.</a>
-            <a href="/" class="footer-link">JYP Ent.</a>
-            <a href="/" class="footer-link">YG Ent.</a>
-            <a href="/" class="footer-link">HYBE Corp.</a>
-            <a href="/" class="footer-link">Cube Ent.</a>
-            <a href="/" class="footer-link">Others</a>
+            <a href="celebs.html" class="footer-link">SM Ent.</a>
+            <a href="celebs.html" class="footer-link">JYP Ent.</a>
+            <a href="celebs.html" class="footer-link">YG Ent.</a>
+            <a href="celebs.html" class="footer-link">HYBE Corp.</a>
+            <a href="celebs.html" class="footer-link">Cube Ent.</a>
+            <a href="celebs.html" class="footer-link">Others</a>
           </div>
           <div class="footer-links-container">
             <h5 class="footer-header">Help</h5>
-            <a href="" class="footer-link">About Us</a>
+            <a href="aboutus.html" class="footer-link">About Us</a>
             <a href="" class="footer-link">Support</a>
             <a href="" class="footer-link">Terms &amp; Conditions</a>
             <a href="" class="footer-link">Privacy Policy</a>
@@ -46,13 +46,13 @@ class Footer extends HTMLElement {
       <div class="footer-bottom">
         <div class="footer-copyright">&copy; Copyright 2023 - Sajuseyo!</div>
         <div class="footer-social">
-          <a href="#" class="footer-social-icon">
+          <a href="www.facebook.com" class="footer-social-icon">
             <i class="fab fa-facebook"></i>
           </a>
-          <a href="#" class="footer-social-icon">
+          <a href="www.instagram.com" class="footer-social-icon">
             <i class="fab fa-instagram"></i>
           </a>
-          <a href="#" class="footer-social-icon">
+          <a href="www.twitter.com" class="footer-social-icon">
             <i class="fab fa-twitter"></i>
           </a>
         </div>
